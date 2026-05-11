65ab960148d84c7f0d7403270bc858a4cbb016cf
yum <wanquanbuhuime@163.com>
2026-05-12 01:23:49 +0800

    feat: add custom type checker Wasm1

---
+++ docs/type-checker/rust_type_checker_bg.wasm.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const check_types: (a: number, b: number) => any;
+export const get_completions: (a: number, b: number, c: number, d: number) => any;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_start: () => void;
